e22f9304af2f64795b451bf0fced43cbca107b60
jennyyu009 <jennyyu009@example.com>
2026-05-28 03:03:12 -0400

    Revert relationship intelligence map dashboard

--- frontend/main.jsx
+++ frontend/main.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import RelationshipIntelligenceMap from "./RelationshipIntelligenceMap";
+import FounderIntakeForm from "./input.jsx";
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RelationshipIntelligenceMap />
+    <FounderIntakeForm />
   </React.StrictMode>,
 );
